3d49164c2dd4e950c7de38f15bddd00b1e99b2e0
Loong <loongy@users.noreply.github.com>
2019-07-12 22:02:28 +0800

    Update examples/lb/lb.go
    Co-Authored-By: ross-pure <32352822+ross-pure@users.noreply.github.com>

--- examples/lb/lb.go
+++ examples/lb/lb.go
@@ -11,7 +11,7 @@ type LB struct {
 	id int
 }
 
-// Handle implements the `phi.Handlerr` interface. We simulate a slow task by
+// Handle implements the `phi.Handler` interface. We simulate a slow task by
 // simply sleeping for a time before returning.
 func (LB) Handle(_ phi.Task, m phi.Message) {
 	if m, ok := m.(Init); ok {
