c4e0161b8b9989feefaf503f84904d2790028813
Vince Au <vinceau09@gmail.com>
2019-08-21 12:02:02 +1000

    test: run ginkgo bootstrap

--- phi_suite_test.go
+++ phi_suite_test.go
@@ -1,1 +1,13 @@
 package phi_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestPhi(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Phi Suite")
+}
