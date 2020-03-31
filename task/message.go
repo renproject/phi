819dde69b6309aa9db1c3b9710ec742fc1a702f5
loongy <loong@renproject.io>
2020-03-31 12:10:39 +1100

    task: simplify commens and remove unused funcs

---
+++ task/message.go
@@ -0,0 +1,48 @@
+package task
+
+import "context"
+
+// Request is a marker interface. It requires no explicit functionality, and
+// only exists so that programmers cannot accidentally send types as requests
+// that were not intended to be requests.
+type Request interface {
+	// IsRequest is a marker function. It should do nothing.
+	IsRequest()
+}
+
+// Response is a marker interface. It requires no explicit functionality, and
+// only exists so that programmers cannot accidentally send types as responses
+// that were not intended to be responses.
+type Response interface {
+	// IsResponse is a marker function. It should do nothing.
+	IsResponse()
+}
+
+// A Message is used for inter-Task communication. It carries an internal
+// Request that will be delivered to the Task.
+type Message interface {
+	// Request returns the inner Request carried by this Message.
+	Request() Request
+
+	// Respond to this Message. Respond should be called at least once on each
+	// Message, but must be called at most once on any Message. It is
+	// non-blocking, and safe for concurrent use.
+	Respond(Response)
+
+	// Wait for a Response to this Message, until the Context is done. Any error
+	// returned from the Context will be returned. Waiting will block the
+	// current goroutine. Waiting is safe for concurrent use, but will only
+	// return a non-nil Response to at most one caller.
+	Wait(context.Context) (Response, error)
+}
+
+// Messages is a collection of messages. Handlers will never receive this type,
+// because it will always be flattened before being sent to the Handler.
+type Messages []Message
+
+// IsMessage implements the Message interface for the Messages type.
+func (Messages) IsMessage() {}
+
+func NewMessage(req Request, res *Response) Message {
+
+}
--- task/task.go
+++ task/task.go
@@ -2,26 +2,7 @@ package task
 
 import (
 	"context"
-
-	"github.com/renproject/phi/parallel"
 )
-
-// Message represents a message that is sent between tasks for communication.
-// It is an enum style interface where a struct can be made a variant of the
-// enum by implementing the interface.
-type Message interface {
-	IsMessage()
-}
-
-// Messages is a collection of messages. Note that handlers will never receive
-// a `Messages` type because they will always be flattened before being
-// processed. If a task needs to respond with more than one message, and it is
-// important that these messages are processed together, then a custom
-// container type should be created and handled appropriately in the handler.
-type Messages []Message
-
-// IsMessage implements the Message interface.
-func (Messages) IsMessage() {}
 
 // Runner represents something that can be run.
 type Runner interface {
@@ -30,120 +11,88 @@ type Runner interface {
 	Run(context.Context)
 }
 
-// Sender represents something that can be sent messages.
+// A Sender is any type that can be sent a Message.
 type Sender interface {
-	// Send takes a message and returns a boolean that indicates if the send was
-	// successful.
-	Send(Message) bool
+	// Send takes a message and attempts to deliver it until the Context is
+	// done. If the Message is successfully delivered before the Context is
+	// done, then a nil error is returned. Otherwise, the Context error is
+	// returned.
+	Send(context.Context, Message) error
 }
 
-// Task is the intersection of the `Runner` and `Sender` interfaces. It
-// represents an entity that (when running) can be sent messages and upon
-// receipt of these messages performs internal logic (which often involves
-// sending messages to other tasks).
+// A Task is any type that can receive Messages while running in another
+// goroutine. It implements Runner (so that it can be run in a background
+// goroutine) and Sender (so that it can be sent Messages).
 type Task interface {
 	Runner
 	Sender
 }
 
-// Handler defines a type that can receive a message and mutate its internal
-// state. The `Task` argument is the parent task of the Handler, and can be used
-// by a Handler to send messages to itself.
+// A Handler is any type that can receive a Message and mutate its own internal
+// state. It is also expected to respond to the Message.
 type Handler interface {
 	Handle(Message)
 }
 
-// Options are passed when constructing a `Task`. The `Cap` is the buffer
-// capacity of the `Task`'s channel, and the `Scale` is the number of worker
-// instances of the handler for load balancing. If `Scale` is an number less
-// than 2, there will only be one instance of the handler. It is important to
-// note that additional copies of the handler will not be created for `Scale`
-// >= 2; this means that handlers that have and modify their own state are not
-// safe to be used at non-unity scales. Only handlers that are purely
-// functional should be used with non-unity scale.
-type Options struct {
-	Cap, Scale int
-}
-
-// task is a basic implementation for a `Task`.
+// A simple implementation of the Task interface. It is sufficient for most
+// purposes.
 type task struct {
-	// The handler for message handling logic.
+	// message handling logic.
 	handler Handler
 
-	// The buffered channel that incoming messages are written to.
+	// input channel for buffering messages that need to be handled.
 	input chan Message
-
-	// The scale (number of workers) for the task.
-	scale int
 }
 
-// New returns a new task with the given handler and buffer capacity. The
-// buffer capacity is the number of messages that can be buffered for
-// processing before the task can no longer accept more messages (until space
-// in the buffer is freed up by processing messages in the buffer).
-func New(handler Handler, opts Options) Task {
+// New Task with the given Handler and buffer capacity. The buffer capacity is
+// the number of Messages that can be buffered for processing before the Task
+// can no longer accept more Messages.
+func New(handler Handler, capacity int) Task {
 	return &task{
 		handler: handler,
-		input:   make(chan Message, opts.Cap),
-		scale:   opts.Scale,
+		input:   make(chan Message, capacity),
 	}
 }
 
-// Run implements the `Runner` interface (in order to implement the `Task`
-// interface). This function blocks. The task will continue to run until it is
-// signalled to terminate by the context.
+// Run the Task until the Context is done. It will read Messages from an
+// internal channel that is filled by calling Send. This method will block the
+// current goroutine until the Context is done.
 func (task *task) Run(ctx context.Context) {
-	loop := func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case message := <-task.input:
-				task.handle(flatten(message))
-			}
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case message := <-task.input:
+			task.handle(flatten(message))
 		}
-	}
-
-	// Don't spawn a go routine if there is no load balancing
-	if task.scale < 2 {
-		loop()
-	} else {
-		parallel.ParForAll(task.scale, func(i int) { loop() })
 	}
 }
 
-// Send implements the `Sender` interface (in order to implement the `Task`
-// interface). It returns a channel to which the (possibly nil) response will
-// be written, and a bool indicating whether the message was able to be sent;
-// true indicates the message was sent, and false indicates that the task
-// currently has a full buffer and won't accept the message.
-func (task *task) Send(m Message) bool {
+// Send a Message to this Task.
+func (task *task) Send(ctx context.Context, message Message) error {
 	select {
-	case task.input <- m:
-		return true
-	default:
-		return false
+	case <-ctx.Done():
+		return ctx.Err()
+	case task.input <- message:
+		return nil
 	}
 }
 
-// handle a message sent to the Task. It is assumed that the message is
-// flattened.
-func (task *task) handle(m Message) {
-	switch m := m.(type) {
+func (task *task) handle(message Message) {
+	switch message := message.(type) {
 	case Messages:
-		for _, msg := range m {
-			task.handler.Handle(msg)
+		for _, message := range message {
+			task.handler.Handle(message)
 		}
 	default:
-		task.handler.Handle(m)
+		task.handler.Handle(message)
 	}
 }
 
-// flatten takes a message and effectively flattens it out to depth 1, where
-// the depth is determined by the level of nested Messages. If `flatten`
-// receives a message that is not a `Messages`, it will return the same
-// message. Otherwise, it will return a `Messages` type where the internal list
-// of messages contain no `Messages`; these will be flattened out.
+// flatten takes a Message and, if it is of the Messages type, flattens it so
+// that none of the elements of the Messages slice are themselves of the
+// Messages type. If flatten receives a Message that is not of the Messages
+// type, then it will return the Message unmodified.
 func flatten(message Message) Message {
 	switch message := message.(type) {
 	case Messages:
